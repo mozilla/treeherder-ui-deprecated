0cd47d42bca067bfca50fe3ee19eecfa8a76c4f4
Cameron Dawson <cdawson@mozilla.com>
2014-09-15 11:11:35 -0700

    progress on repo-menu events for showing/hiding

--- webapp/app/js/controllers/repository.js
+++ webapp/app/js/controllers/repository.js
@@ -10,9 +10,29 @@ treeherder.controller('RepositoryMenuCtrl', [
         $scope.closeable = true;
 
         $('.dropdown.keep-open').on({
-            "shown.bs.dropdown": function() { $scope.closable = false; },
-            "click":             function() { $scope.closable = true; },
-            "hide.bs.dropdown":  function() { return $scope.closable; }
+            "shown.bs.dropdown": function(ev) {
+                $scope.closeable = false;
+            },
+            "click":             function(ev) {
+                $scope.closeable = true;
+            },
+            "hide.bs.dropdown":  function(ev) {
+                var closeable = $scope.closeable;
+                $scope.closeable = true;
+                return closeable;
+            }
+        });
+
+        $('.repo-dropdown-menu').on({
+            "click":             function(ev) {
+                $scope.closeable = true;
+            },
+            "mouseup":             function(ev) {
+                $scope.closeable = false;
+            },
+            "hide.bs.dropdown":  function(ev) {
+                return $scope.closeable;
+            }
         });
 
     }
